eee40e5319fc1de8c152c6e10d78c11d957daae4
CoolNight99 <timmyscottmy@gmail.com>
2025-01-10 21:01:52 +0800

    made changes

--- docs/script.js
+++ docs/script.js
@@ -1,38 +1,23 @@
 const faqQuestionDivs = document.querySelectorAll(".faq-question-div");
 
+const toggleFAQ = (faqAnswer, plusIcon) => {
+    const isVisible = window.getComputedStyle(faqAnswer).display !== "none";
+    faqAnswer.style.display = isVisible ? "none" : "block";
+    plusIcon.src = isVisible
+        ? "./assets/images/icon-plus.svg"
+        : "./assets/images/icon-minus.svg";
+};
+
 faqQuestionDivs.forEach((faqQuestionDiv) => {
     const plusIcon = faqQuestionDiv.querySelector(".plus-icon");
     const faqAnswer = faqQuestionDiv.nextElementSibling;
 
     faqAnswer.style.display = "none";
 
-    plusIcon.addEventListener("click", () => {
-        const isVisible = window.getComputedStyle(faqAnswer).display !== "none";
+    const handleToggle = () => toggleFAQ(faqAnswer, plusIcon);
 
-        if (!isVisible) {
-            faqAnswer.style.display = "block"; 
-            plusIcon.src = "./assets/images/icon-minus.svg";
-        } 
-        
-        else {
-            faqAnswer.style.display = "none";
-            plusIcon.src = "./assets/images/icon-plus.svg";
-        }
-    });
-
-    plusIcon.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            const isVisible = window.getComputedStyle(faqAnswer).display !== "none";
-
-            if (!isVisible) {
-                faqAnswer.style.display = "block"; 
-                plusIcon.src = "./assets/images/icon-minus.svg";
-            } 
-            
-            else {
-                faqAnswer.style.display = "none";
-                plusIcon.src = "./assets/images/icon-plus.svg";
-            }
-        }
+    plusIcon.addEventListener("click", handleToggle);
+    plusIcon.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") handleToggle();
     });
 });
